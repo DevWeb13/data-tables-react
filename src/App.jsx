da078b8725374b179b8bffc26b07079a8d2dfe12
DevWeb13 <giuliano.loic@sfr.fr>
2022-05-18 21:20:29 +0200

    fix: (App.jsx): fix bug option value

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App({ employees }) {
               value={selectButtonValue}
               onChange={handleChange}
             >
-              <option value={10}>{selectButtonValue}</option>
+              <option value={10}>10</option>
               <option value={25}>25</option>
               <option value={50}>50</option>
               <option value={100}>100</option>
